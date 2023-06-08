5bb43c0942d93e21e1d27f6d626f352689f6f069
kodexx <65950405+pintoedu@users.noreply.github.com>
2023-06-08 11:32:58 -0400

    Update rotation30.py

--- rotation30.py
+++ rotation30.py
@@ -8,7 +8,6 @@ client_secret = "your_client_secret"
 redirect_uri = "your_redirect_uri"
 username = "your_username"
 
-
 # Authenticate and create Spotipy client
 auth_manager = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope="playlist-read-private playlist-modify-public")
 sp = spotipy.Spotify(auth_manager=auth_manager)
